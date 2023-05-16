59d152e288f5fdeda71e272e4521e875be41eb12
Srini <srinivasboini7@gmail.com>
2023-05-16 19:59:38 +0800

    Initial commit

---
+++ src/main/java/com/srini/timelimiter/Resilience4jTimelimiterApplication.java
@@ -0,0 +1,13 @@
+package com.srini.timelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jTimelimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jTimelimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srini/timelimiter/Resilience4jTimelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srini.timelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jTimelimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
